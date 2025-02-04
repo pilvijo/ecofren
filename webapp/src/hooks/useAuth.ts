ccccf964934ab08c2dc631afae2db57da5f6790f
intheloop <admin@tiscacatalin.com>
2025-02-04 15:12:54 +0200

    useAuth hook

---
+++ webapp/src/hooks/useAuth.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import supabase from '../utils/supabase';
+
+export default function useAuth() {
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setUser(session?.user ?? null);
+    });
+
+    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+      setUser(session?.user ?? null);
+    });
+
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
+  }, []);
+
+  return { user };
+}
